4641e8c113ca6b1abf29eed9599ea7c47cbbec29
GuglielmoBini <guglielmobini@gmail.com>
2023-03-13 17:10:25 +0100

    migrated and seeded Technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['label' => 'HTML', 'color' => 'danger'],
+            ['label' => 'CSS', 'color' => 'info'],
+            ['label' => 'Bootstrap', 'color' => 'primary-emphasis'],
+            ['label' => 'JavaScript', 'color' => 'warning'],
+            ['label' => 'Vue', 'color' => 'success'],
+            ['label' => 'SASS', 'color' => 'dark'],
+            ['label' => 'PHP', 'color' => 'primary'],
+            ['label' => 'MySQL', 'color' => 'secondary-emphasis'],
+            ['label' => 'Laravel', 'color' => 'danger-emphasis'],
+        ];
+
+        foreach ($technologies as $technology) {
+            $new_tech = new Technology();
+            $new_tech->label = $technology['label'];
+            $new_tech->color = $technology['color'];
+            $new_tech->save();
+        }
     }
 }
