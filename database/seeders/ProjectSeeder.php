7a88568fef695424181baf0abf2bdb14644e2f30
GuglielmoBini <guglielmobini@gmail.com>
2023-03-13 20:11:20 +0100

    randomized technologies

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,8 @@ class ProjectSeeder extends Seeder
         $type_ids = Type::select('id')->pluck('id')->toArray();
         $type_ids[] = null;
 
+        $technology_ids = Technology::select('id')->pluck('id')->toArray();
+
         for ($i = 0; $i < 30; $i++) {
             $project = new Project();
             $project->type_id = Arr::random($type_ids);
@@ -27,6 +30,13 @@ class ProjectSeeder extends Seeder
             // $project->image_url = $faker->imageUrl(250, 250);
             $project->description = $faker->paragraphs(5, true);
             $project->save();
+
+            $project_technologies = [];
+            foreach ($technology_ids as $technology_id) {
+                if ($faker->boolean()) $project_technologies[] = $technology_id;
+            }
+
+            $project->technologies()->attach($project_technologies);
         }
     }
 }
